dd78dde4151091ecc03794ecb97ec2538610bd8f
yuto.eguma <yuto.eguma@gmail.com>
2021-01-26 22:21:53 +0900

    first commit

---
+++ src/main/java/io/cabos/demo/controller/SampleController.java
@@ -0,0 +1,13 @@
+package io.cabos.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+
+    @GetMapping
+    public SampleDTO index() {
+        return new SampleDTO("OK");
+    }
+}
---
+++ src/main/java/io/cabos/demo/controller/SampleDTO.java
@@ -0,0 +1,10 @@
+package io.cabos.demo.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class SampleDTO {
+    private final String message;
+}
