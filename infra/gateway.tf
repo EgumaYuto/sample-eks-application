5fb6e84f0b953852a3f03c52a9337d4e98ff0dba
yuto.eguma <yuto.eguma@gmail.com>
2021-02-06 00:19:55 +0900

    API Gateway を空で作成

---
+++ infra/gateway.tf
@@ -0,0 +1,45 @@
+resource "aws_api_gateway_rest_api" "demo" {
+  name = local.name
+
+  body = jsonencode({
+    openapi = "3.0.1"
+    info = {
+      title = "example"
+      version = "1.0"
+    }
+    paths = {
+      "/" = {
+        get = {
+          x-amazon-apigateway-integration = {
+            httpMethod           = "GET"
+            payloadFormatVersion = "1.0"
+            type                 = "HTTP_PROXY"
+            uri                  = "https://ip-ranges.amazonaws.com/ip-ranges.json"
+          }
+        }
+      }
+    }
+  })
+
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
+}
+
+resource "aws_api_gateway_deployment" "deployment" {
+  rest_api_id = aws_api_gateway_rest_api.demo.id
+
+  triggers = {
+    redeployment = sha1(jsonencode(aws_api_gateway_rest_api.demo.body))
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_api_gateway_stage" "v1" {
+  deployment_id = aws_api_gateway_deployment.deployment.id
+  rest_api_id   = aws_api_gateway_rest_api.demo.id
+  stage_name    = "v1"
+}
