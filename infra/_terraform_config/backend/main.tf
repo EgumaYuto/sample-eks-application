699c9c336b5d3fe2d76c30d6c69bb5d40fd04003
yuto.eguma <yuto.eguma@gmail.com>
2021-04-07 00:47:47 +0900

    state ファイル管理バケットを強化

--- infra/_terraform_config/backend/main.tf
+++ infra/_terraform_config/backend/main.tf
@@ -1,5 +1,10 @@
 resource "aws_s3_bucket" "bucket" {
   bucket = local.name
+
+  force_destroy = local.force_destroy
+  versioning {
+    enabled = local.versioning
+  }
 }
 
 resource "aws_s3_bucket_public_access_block" "bucket" {
