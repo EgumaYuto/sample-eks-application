d29e148d181f63ede4002f494366fd07134de1b3
yuto.eguma <yuto.eguma@gmail.com>
2021-04-07 00:55:14 +0900

    subnetの作成

---
+++ infra/platform/network/main/subnet/public/data.tf
@@ -0,0 +1,9 @@
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+
+  config = {
+    bucket = var.state_bucket
+    key    = "env:/${terraform.workspace}/state/platform/network/main/vpc.tfstate"
+    region = var.default_region
+  }
+}
---
+++ infra/platform/network/main/subnet/public/main.tf
@@ -0,0 +1,36 @@
+terraform {
+  backend "s3" {
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+module "naming" {
+  source = "../../../../../_module/naming"
+  role   = local.role
+}
+
+resource "aws_subnet" "subent" {
+  count = length(var.default_availability_zones)
+
+  vpc_id            = local.vpc_id
+  cidr_block        = cidrsubnet(local.cidr_block, local.newbits, 0 + count.index)
+  availability_zone = element(var.default_availability_zones, count.index)
+
+  tags = module.naming.tags
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = local.vpc_id
+}
+
+resource "aws_route_table" "route_table" {
+  vpc_id = local.vpc_id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
