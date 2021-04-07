986d507024d1bec941aa5c5e9be233b5f365a5d3
yuto.eguma <yuto.eguma@gmail.com>
2021-04-07 13:43:02 +0900

    create destroy を簡単にするスクリプトの追加

---
+++ infra/tf_destroy_all.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+TF_CMD=$1
+ENV=$2
+TF_ARGS=${@:3}
+
+./tf.sh $TF_CMD $ENV platform/network/main/subnet/private $TF_ARGS
+./tf.sh $TF_CMD $ENV platform/network/main/subnet/public $TF_ARGS
+./tf.sh $TF_CMD $ENV platform/network/main/vpc $TF_ARGS
