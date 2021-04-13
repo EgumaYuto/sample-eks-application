79824baf3639d62b3d1edf030c5cf8d4ab7fc4d5
yuto.eguma <yuto.eguma@gmail.com>
2021-04-14 00:58:31 +0900

    create/destroy all を修正

--- infra/tf_destroy_all.sh
+++ infra/tf_destroy_all.sh
@@ -4,9 +4,12 @@ TF_CMD=$1
 ENV=$2
 TF_ARGS=${@:3}
 
+./tf.sh $TF_CMD $ENV service/sample-api/api-gateway $TF_ARGS
+./tf.sh $TF_CMD $ENV service/sample-api/ecs $TF_ARGS
+./tf.sh $TF_CMD $ENV service/sample-api/elb $TF_ARGS
 ./tf.sh $TF_CMD $ENV service/sample-api/ecr $TF_ARGS
 
 ./tf.sh $TF_CMD $ENV platform/ecs-cluster/main $TF_ARGS
-./tf.sh $TF_CMD $ENV platform/network/service/main/private $TF_ARGS
-./tf.sh $TF_CMD $ENV platform/network/service/main/public $TF_ARGS
-./tf.sh $TF_CMD $ENV platform/network/service/vpc $TF_ARGS
+./tf.sh $TF_CMD $ENV platform/network/main/subnet/private $TF_ARGS
+./tf.sh $TF_CMD $ENV platform/network/main/subnet/public $TF_ARGS
+./tf.sh $TF_CMD $ENV platform/network/main/vpc $TF_ARGS
