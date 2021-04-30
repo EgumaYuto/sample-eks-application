ad52e540d630b2320aed6c9826ef0cc213af5ff7
yuto.eguma <yuto.eguma@gmail.com>
2021-04-30 14:11:43 +0900

    ECRのprimary env 出ない環境を考慮

--- infra/service/sample-api/ecr/outputs.tf
+++ infra/service/sample-api/ecr/outputs.tf
@@ -1,3 +1,16 @@
 output "name" {
   value = local.name
 }
+
+output "arn" {
+  value = var.primary_env ? aws_ecr_repository.repository[0].arn : data.aws_ecr_repository.repository.arn
+}
+
+output "repository_url" {
+  value = var.primary_env ? aws_ecr_repository.repository[0].repository_url : data.aws_ecr_repository.repository.repository_url
+}
+
+data "aws_ecr_repository" "repository" {
+  count = var.primary_env ? 0 : 1
+  name  = local.name
+}
