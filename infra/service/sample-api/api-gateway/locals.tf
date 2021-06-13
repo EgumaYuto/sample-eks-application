2900ec5fa10a459b990629e3e6d04fc90e7ad058
yuto.eguma <yuto.eguma@gmail.com>
2021-06-13 23:47:23 +0900

    不要な変数を削除

--- infra/service/sample-api/api-gateway/locals.tf
+++ infra/service/sample-api/api-gateway/locals.tf
@@ -2,6 +2,5 @@ locals {
   role             = "sample-api"
   vpc_id           = data.terraform_remote_state.vpc.outputs.id
   subnet_ids       = data.terraform_remote_state.private_subnet.outputs.ids
-  target_group_arn = data.terraform_remote_state.elb.outputs.target_group.arn
   listener_arn     = data.terraform_remote_state.elb.outputs.listener.arn
 }
